08486c95fb2698af49ef7c0addbcba931b7aabd7
blfuentes <epiphonetodie@gmail.com>
2020-11-24 08:48:13 +0100

    day 01 start

--- day01/part01/day01_part01.fs
+++ day01/part01/day01_part01.fs
@@ -1,4 +1,11 @@
 ﻿module day01_part01
 
-let execute =
-    0
+open Utilities
+
+let path = "test_input_00.txt"
+
+let execute =    
+    let lines = GetLinesFromFile(path)
+    let elements = lines |> Seq.head |> Seq.toList
+    elements
+
--- day01/part01/day01_part01.fsx
+++ day01/part01/day01_part01.fsx
@@ -1,1 +1,7 @@
-﻿
+﻿open System.IO
+
+let path = "test_input_00.txt"
+let lines = File.ReadLines(__SOURCE_DIRECTORY__ + @"../../" + path)
+
+let elements = lines |> Seq.head |> Seq.toList
+elements
